ff2ff011b7c37d0f72de39c3215f048e352e1eef
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-07-23 21:00:55 +0200

    Add todo list

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import SearchBar from './components/SearchBar'
+import {Todo} from './model'
 
 
 const App: React.FC = () => {
+  const [todo, setTodo] = React.useState<string>("")
+  const [todos, setTodos] = React.useState<Todo[]>([])
+
+const handleAddTodo = (e:React.FormEvent<HTMLFormElement>) =>{
+  e.preventDefault();
+
+  if (todo){
+    setTodos(
+      [...todos,
+      {id: Date.now(), todo, isDone: false}]);
+      setTodo("");
+  }
+}
 
   return (
     <div className='container'>
       <h1 className="text-3xl font-bold text-center m-5">
         Nota
       </h1>
-      <SearchBar />
+      <SearchBar todo={todo} setTodo={setTodo} handleAddTodo={handleAddTodo} />
+      {todos.map(item => (
+        <li key={item.id}>{item.id} | {item.todo} | {item.isDone ? 'completed' : 'open'}</li>
+      ))}
     </div>
   )
 }
--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-const SearchBar = () => {
+interface TodoProps{
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAddTodo: (e:React.FormEvent<HTMLFormElement>) => void;
+}
+
+const SearchBar: React.FC<TodoProps> = (props) => {
+
+const handleChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
+    props.setTodo(e.target.value)
+}
+
   return (
-    <form action="">
+    <form action="" onSubmit={props.handleAddTodo}>
         <label className="relative block w-[50%] mx-auto">
             <span className="sr-only">Search</span>
             <span className="absolute inset-y-0 left-0 flex items-center pl-2">
@@ -10,13 +21,14 @@ const SearchBar = () => {
             </span>
             <input 
             className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-full py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-slate-500 focus:ring-slate-500 focus:ring-1 sm:text-sm" 
-            placeholder="Search for anything..." 
-            type="text" 
+            placeholder="Add item to todo-list" 
+            type="search"
+            onChange={handleChange}
+            value={props.todo}
             name="search"/>
             <input 
-            className='absolute inset-y-0 right-0 border bg-slate-700 text-white rounded-full px-2 sm:text-sm'
-            value='search'
-
+            className='absolute inset-y-0 right-0 bg-slate-700 border border-slate-700 text-white rounded-r-full px-2 sm:text-sm'
+            value='push'
             type="submit" />
         </label>
     </form>
